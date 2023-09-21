d5a27e36197c8c57fba65d97e1666285d6844b52
prateekvarma <prtk.varma@gmail.com>
2023-09-21 14:10:45 -0600

    Added styling to nav and changed global styles

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -6,24 +6,27 @@ import Image from "next/image";
 
 export default function Nav({ user }: Session) {
   return (
-    <nav>
+    <nav className="flex justify-between items-center py-8">
       <h1>styled</h1>
-      <ul>
-        <li>Products</li>
+      <ul className="flex items-center gap-12">
         {!user && (
-          <li>
+          <li className="bg-teal-600 text-white py-2 px-4 rounded-md">
             <button onClick={() => signIn()}>Sign in</button>
           </li>
         )}
         {user && (
-          <li>
-            <Image
-              src={user?.image as string}
-              alt={user?.name as string}
-              width={48}
-              height={48}
-            />
-          </li>
+          <>
+            <li>
+              <Image
+                src={user?.image as string}
+                alt={user?.name as string}
+                width={48}
+                height={48}
+                className="rounded-full"
+              />
+            </li>
+            <li>Dashboard</li>
+          </>
         )}
       </ul>
     </nav>
